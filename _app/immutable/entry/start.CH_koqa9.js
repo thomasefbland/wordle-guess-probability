43ce2a31abe6c9b65a1764c4f86603e62d83d5f7
Thomas Bland <thomasefbland@gmail.com>
2024-10-16 00:25:36 -0400

    Updates

---
+++ _app/immutable/entry/start.CH_koqa9.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.lsaXtAWh.js";export{t as start};
--- src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import "../app.postcss";
-
-	let { children } = $props();
-</script>
-
-{@render children()}
